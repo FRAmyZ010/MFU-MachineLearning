82de612c2db63006562b89501eb3bca661bc6d08
Pongsapat P. <tadzyz010@gmail.com>
2025-08-11 08:30:21 +0700

    a

--- [2]matplotlib1.py
+++ [2]matplotlib1.py
@@ -7,3 +7,31 @@ import matplotlib.pyplot as plt
 # plt.show() # สั่งแสดงกราฟ
 
 
+# ==============================================
+#               การปรับขนาดกราฟ
+# ==============================================
+
+# y = np.random.randint(1,11,5) # สร้าง array ที่มีตัวเลขตั้งแต่ 1 - 10 จำนวน 5 ตัว
+# x = list('ABCDE') # แปลง String ให้กลายเป็น array list ['A','B','C','D','E']
+
+# plt.rcParams['figure.figsize'] = (10,5) #width = 10" , height = 5"
+
+# # หรืออีกแบบคือ
+# # plt.figure(figsize=(10,5))
+
+# plt.plot(x,y)
+# plt.show()
+
+# ========== Function style.use()
+
+# plt.style.use('seaborn-v0_8')
+# y = np.random.randint(1,11,5) # สร้าง array ที่มีตัวเลขตั้งแต่ 1 - 10 จำนวน 5 ตัว
+# x = list('ABCDE') # แปลง String ให้กลายเป็น array list ['A','B','C','D','E']
+
+# # print(plt.style.available) เพื่อเช็ครายชื่อ style
+
+# plt.plot(x,y)
+# plt.show()
+
+
+
